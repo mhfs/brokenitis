61e7f370d8e7ecba274ad5c7814a533535cc3f44
Marcelo Silveira <marcelo@mhfs.com.br>
2014-05-27 21:01:52 -0700

    add form

--- main.js
+++ main.js
@@ -4,16 +4,41 @@ $.urlParam = function(name){
   return results[1] || 0;
 }
 
+function buildURL(subject){
+  return 'http://brokenitis.com?subject=' + encodeURIComponent(subject);
+}
+
 $(function(){
   var subject = $.urlParam("subject");
   var timeout = $.urlParam("timeout");
 
   if(subject) {
     $('.subject').text(decodeURIComponent(subject));
+
+    setTimeout(function(){
+      $('#answer').show();
+      $('#processing').hide();
+    }, timeout || 1500);
+  }
+  else {
+    $('#form').show();
   }
 
-  setTimeout(function(){
-    $('#answer').show();
-    $('#processing').hide();
-  }, timeout || 1500);
+  $("#subject_input").on('keyup', function(){
+    var url = buildURL($(this).val());
+    $("#url_output").val(url);
+  });
+
+  $("#url_output").on('mouseup', function() {
+    $(this).select();
+  });
+
+  $("#show_link").on('click', function(){
+    $(this).hide();
+    $('#form').show();
+    $('#subject_input').val(decodeURIComponent(subject)).focus().select();
+
+    var url = buildURL(subject);
+    $("#url_output").val(url);
+  });
 });
