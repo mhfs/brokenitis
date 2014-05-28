404d50ba0e3a33840925b71e264bc8156f7560c1
Marcelo Silveira <marcelo@mhfs.com.br>
2014-05-27 20:11:16 -0700

    stupid silly page

---
+++ main.js
@@ -0,0 +1,16 @@
+$.urlParam = function(name){
+  var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (!results) { return 0; }
+  return results[1] || 0;
+}
+
+$(function(){
+  var subject = $.urlParam("subject");
+  var timeout = $.urlParam("timeout");
+  $('.subject').text(decodeURIComponent(subject));
+
+  setTimeout(function(){
+    $('#answer').show();
+    $('#processing').hide();
+  }, timeout || 1500);
+});
